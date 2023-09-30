6bd1842b53c88d7b0999878233cb737faeb58d50
Vinicius Biazi Bastos <145498513+Patus-Genialis@users.noreply.github.com>
2023-09-30 12:00:21 -0300

    Update Check_in.js

--- Check_in.js
+++ Check_in.js
@@ -1,6 +1,6 @@
 function check_in() {
     var cpf = "cpf collum";
-    var control = [];
+    var id = "ID collum";
 
     var ws = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("SpreadsheetName");
     var data = ws.getRange("B2:O" + ws.getLastRow()).getValues();
@@ -11,7 +11,7 @@ function check_in() {
     data.forEach(function(row){
       for (var i = 0; i <= ck.getLastRow(); i = i + 1){
         if (row[cpf] == checked[i]){
-          ws.getRange(row[control], 14).setValue(true);
+          ws.getRange(row[id], "check_in collum").setValue(true);
           break
         }
       }
